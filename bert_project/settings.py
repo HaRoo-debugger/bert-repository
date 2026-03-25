ecbba57311f6a57f09c43c896e2faae0d4e9948b
Bert Nichole Estrella <bertestrella@gmail.com>
2026-03-25 21:42:29 +0800

    Configure allowed hosts for deployment

--- bert_project/settings.py
+++ bert_project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bertnichole_estrella.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
